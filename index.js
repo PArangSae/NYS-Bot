d472361e365661b7664d0fd06b115bac1db88155
WooyeongCho <nyangiwy24@naver.com>
2018-08-28 20:39:12 +0900

    asdf

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require("./config.json"); //nysbothello
+const config = require("./config.json");
 const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
