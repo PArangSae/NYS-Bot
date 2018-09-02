7c87f133c40eae90894c3221699cce2847ea27bd
WooyeongCho <nyangiwy24@naver.com>
2018-09-02 17:30:38 +0900

    d

--- index.js
+++ index.js
@@ -192,26 +192,27 @@ bot.on("message", async message => {
       }
 
     case `${prefix}tr`:
-      app.get('/translate', function (req, res) {
-        var api_url = 'https://openapi.naver.com/v1/language/translate';
-        var options = {
-            url: api_url,
-            form: {'source':'ko', 'target':'en', 'text':"안녕하세요. 저는 고양이를 좋아합니다."},
-            headers: {'X-Naver-Client-Id':config.tr_client_id, 'X-Naver-Client-Secret': config.tr_secret}
-          };
-          request.post(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-              res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-              res.end(body);
-            } else {
-              res.status(response.statusCode).end();
-              console.log('error = ' + response.statusCode);
-            }
-          });
-        });
-      app.listen(3000, function () {
-        console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
-        });
+    app.get('/translate', function (req, res) {
+       var api_url = 'https://openapi.naver.com/v1/language/translate';
+       var request = require('request');
+       var options = {
+           url: api_url,
+           form: {'source':'ko', 'target':'en', 'text':allArgs},
+           headers: {'X-Naver-Client-Id':config.tr_client_id, 'X-Naver-Client-Secret': config.tr_secret}
+        };
+       request.post(options, function (error, response, body) {
+         if (!error && response.statusCode == 200) {
+           res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+           res.end(body);
+         } else {
+           res.status(response.statusCode).end();
+           console.log('error = ' + response.statusCode);
+         }
+       });
+     });
+     app.listen(3000, function () {
+       console.log('http://127.0.0.1:3000/translate app listening on port 3000!');
+     });
       break;
 
     default: //구문이 잘못된 경우
